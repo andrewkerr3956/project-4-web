6d915e1de57ebbb5595e4bbaccdf3f4d9c8f43c9
Andrew Kerr <andrewkerr3956@gmail.com>
2021-12-09 15:43:10 -0500

    Began working on the design for the page.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+
+const ApiComponent = () => {
+  return (
+    <div style={{textAlign: "center"}}>
+      Test
+    </div>
+  )
+}
+
+const DbComponent = () => {
+  return (
+    <div style={{textAlign: "center"}}>
+      Test
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={"main-container"}>
+      <header>
+        <div className={"header-container"}>
+          <h2>Paper Trader</h2>
+          <h4><em>We make buying shares as easy as it should be.</em></h4>
+        </div>
       </header>
+      <main>
+        <div className={"flex-container"}>
+          <div className={"api-container"}>
+            <ApiComponent />
+          </div>
+          <div className={"database-container"}>
+            <DbComponent />
+          </div>
+        </div>
+      </main>
+      <footer>
+
+      </footer>
     </div>
   );
 }

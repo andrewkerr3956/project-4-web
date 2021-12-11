acce62ee52fe31526f19306456d742668952f611
Andrew Kerr <andrewkerr3956@gmail.com>
2021-12-11 00:04:28 -0500

    Made some more progress on the design

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import './App.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 const ApiComponent = () => {
+  const [search, setSearch] = useState("");
+  const handleSearch = (event) => {
+    let newSearch = event.target.value;
+    setSearch(newSearch)
+  }
   return (
-    <div style={{textAlign: "center"}}>
-      Test
-    </div>
+    <>
+      <div style={{display: "flex", justifyContent: "center"}}>
+        <input type="text" className={"search-bar"} name="search" value={search} onChange={handleSearch} placeholder={"Search..."}/>
+        <button className={"search-btn"}><FontAwesomeIcon icon={faSearch} /></button>
+      </div>
+      <section name="stocks-section">
+        <div className={"stocks-container"}>
+
+        </div>
+      </section>
+
+    </>
   )
 }
 
 const DbComponent = () => {
   return (
-    <div style={{textAlign: "center"}}>
-      Test
-    </div>
+    <>
+      
+    </>
   )
 }
 
 function App() {
   return (
     <div className={"main-container"}>
+      {/*In case the screen is too small */}
+      <h3 className={"small-screen-warning"}>The app cannot work on this device.</h3>
       <header>
         <div className={"header-container"}>
           <h2>Paper Trader</h2>
-          <h4><em>We make buying shares as easy as it should be.</em></h4>
+          <h4><em>We make buying stocks as easy as it should be.</em></h4>
         </div>
       </header>
       <main>

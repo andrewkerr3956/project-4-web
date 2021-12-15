e04335998e8e3c52544ff26460e6b575e4ce12e9
Andrew Kerr <andrewkerr3956@gmail.com>
2021-12-15 17:12:58 -0500

    Login/Register buttons now disappear when logging in, the yahoo stock prices API now displays the symbol and price, and also, when logged in, the session is stored and will display 'Hello, (username)' with username being the logged in user.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 
@@ -9,23 +9,32 @@ const ApiComponent = () => {
     let newSearch = event.target.value;
     setSearch(newSearch)
   }
-  const fetchSearch = async() => {
+  const fetchSearch = async () => {
     let symbol = search;
     console.log(`http://localhost:3000/api/search/${symbol}`)
     let data = await fetch(`http://localhost:3000/api/search/${symbol}`);
     data = await data.json();
-    console.log(data);
+    if (data.error) {
+      return alert("Data for this symbol could not be found.");
+    }
+    else {
+      document.getElementById("yahoo-info").innerHTML =
+        `<span>
+      <span>${symbol}</span>
+      <span>$${data.data.price.toFixed(2)}</span>
+    </span>`
+    }
   }
   return (
     <>
-      <div style={{display: "flex", justifyContent: "center"}}>
-        <input type="text" className={"search-bar"} name="search" value={search} onChange={handleSearch} placeholder={"Search..."}/>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <input type="text" className={"search-bar"} name="search" value={search} onChange={handleSearch} placeholder={"Search..."} />
         <button className={"search-btn"} onClick={fetchSearch}><FontAwesomeIcon icon={faSearch} /></button>
       </div>
       <section name="stocks-section">
         <div className={"stocks-container"}>
-          
-        </div>
+          <div id="yahoo-info"></div>
+        </div>  
       </section>
 
     </>
@@ -37,10 +46,10 @@ const DbComponent = () => {
     <>
       <h3>Portfolio</h3>
       <div className={'grid-container'}>
-            <div className={'grid-header'}><strong>Stock</strong></div>
-            <div className={'grid-header'}><strong>Quantity</strong></div>
-            <div className={'grid-header'}><strong>Value</strong></div>
-            <div className={'grid-header'}><strong>Buy/Sell</strong></div>
+        <div className={'grid-header'}><strong>Stock</strong></div>
+        <div className={'grid-header'}><strong>Quantity</strong></div>
+        <div className={'grid-header'}><strong>Value</strong></div>
+        <div className={'grid-header'}><strong>Buy/Sell</strong></div>
       </div>
     </>
   )
@@ -52,8 +61,8 @@ function App() {
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
 
-  const loginBox = () => { 
-    if(registerDisplay) {
+  const loginBox = () => {
+    if (registerDisplay) {
       setRegisterDisplay(false);
     }
     let newDisplay = !loginDisplay;
@@ -61,7 +70,7 @@ function App() {
   }
 
   const registerBox = () => {
-    if(loginDisplay) {
+    if (loginDisplay) {
       setLoginDisplay(false);
     }
     let newDisplay = !registerDisplay;
@@ -78,29 +87,50 @@ function App() {
     setPassword(newPassword);
   }
 
-  const handleLogin = async() => {
-    let success = await fetch('http://localhost:3000/api/portfolio/', { 
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    let login = await fetch('http://localhost:3000/api/portfolio/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({username: username, password: password})
-      })
-    success = await success.json();
-    console.log(success);
+      body: JSON.stringify({ username: username, password: password })
+    });
+    login = await login.json();
+    console.log(login);
+    if (login.error) {
+      return alert("Invalid login, please try again.");
+    }
+    else {
+      sessionStorage.setItem("userid", login.results[0].userid);
+      sessionStorage.setItem("user", login.results[0].username);
+      window.location.reload();
+    }
+  }
+
+  const handleLogout = async (event) => {
+    sessionStorage.removeItem("userid");
+    sessionStorage.removeItem("user");
+    window.location.reload();
   }
 
 
-  const handleRegister = async() => {
-    let success = await fetch('http://localhost:3000/api/portfolio/', { 
+  const handleRegister = async (event) => {
+    event.preventDefault();
+    let register = await fetch('http://localhost:3000/api/portfolio/', {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({username: username, password: password})
-      })
-    success = await success.json();
-    console.log(success);
+      body: JSON.stringify({ username: username, password: password })
+    });
+    register = await register.json();
+    if(register.error) {
+      return alert("Username already exists!");
+    }
+    else {
+      window.location.reload();
+    }
   }
 
   return (
@@ -109,35 +139,37 @@ function App() {
       <h3 className={"small-screen-warning"}>The app cannot work on this device.</h3>
       <header>
         <div className={"header-container"}>
-          {sessionStorage.username && ((sessionStorage) => {
-            const userName = sessionStorage.username;
-            const id = sessionStorage.userid;
-            return (
-              <div key={id}>Welcome, {userName}!</div>
-            )
-          })}
-          <h2>Paper Trader</h2>
+          <h2 style={{ margin: "0 auto" }}>Paper Trader</h2>
           <h4><em>We make buying stocks as easy as it should be.</em></h4>
           <div className={'login-container'}>
-            <button onClick={loginBox} style={{backgroundColor: "blue"}}>Login</button>
-            <button onClick={registerBox} style={{backgroundColor: "green"}}>Register</button>
+            {sessionStorage.length > 0 && (
+              <div key={sessionStorage.getItem("userid")} style={{ fontWeight: "bold" }}>Welcome, {sessionStorage.getItem("user")}!
+                <button onClick={handleLogout} style={{ backgroundColor: "crimson", color: "white", marginLeft: "10px" }}>Logout</button>
+              </div>
+            )}
+            {sessionStorage.length === 0 && (
+              <>
+                <button onClick={loginBox} style={{ backgroundColor: "blue" }}>Login</button>
+                <button onClick={registerBox} style={{ backgroundColor: "green" }}>Register</button>
+              </>
+            )}
             {loginDisplay && (
-                <div className={"login-box"}>
-                  <form>
-                    <input type="text" value={username} onChange={handleUsername} placeholder={"Enter username..."} required />
-                    <input type="password" value={password} onChange={handlePassword} placeholder={"Enter password..."} required />
-                    <button onClick={handleLogin}>Login</button>
-                  </form>
-                </div>
+              <div className={"login-box"}>
+                <form>
+                  <input type="text" value={username} onChange={handleUsername} placeholder={"Enter username..."} required maxLength={50} />
+                  <input type="password" value={password} onChange={handlePassword} placeholder={"Enter password..."} required />
+                  <button onClick={handleLogin}>Login</button>
+                </form>
+              </div>
             )}
             {registerDisplay && (
-                <div className={"login-box"}>
-                  <form>
-                    <input type="text" value={username} onChange={handleUsername} placeholder={"Enter username..."} required />
-                    <input type="password" value={password} onChange={handlePassword} placeholder={"Enter password..."} required />
-                    <button onClick={handleRegister} style={{backgroundColor: 'lightseagreen'}}>Register</button>
-                  </form>
-                </div>
+              <div className={"login-box"}>
+                <form>
+                  <input type="text" value={username} onChange={handleUsername} placeholder={"Enter username..."} required maxLength={50} />
+                  <input type="password" value={password} onChange={handlePassword} placeholder={"Enter password..."} required />
+                  <button onClick={handleRegister} style={{ backgroundColor: 'lightseagreen' }}>Register</button>
+                </form>
+              </div>
             )}
           </div>
         </div>
